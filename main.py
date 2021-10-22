47553da6fe0e7e4d30f2a12610c74a97b37a3c2a
A00833604 <88684997+A00833604@users.noreply.github.com>
2021-10-21 22:47:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,95 +1,127 @@
-ventas = []
-articulos = []
-vendedores = [['Placeholder']]
-vendedor_articulo = []
-cantidad_articulo =[]
-inventario_articulos = []
-#agrega un articulo a la lista ventas, quita el articulo de la lista articulos
-def def_art_vendido(articulo):
-    ventas.append(articulo)
-    if(articulo in articulos):
-        articulos.remove(articulo)
+#solo falta ponerlo en un mega while loop, (punto 7 y 8)
+nombre_vendedor = ['Pablo', 'Julieta', 'Rolando']
+datos_ventas_vendedor = [[0,2,0,1,0,0,0], [1,0,0,0,0,2,3], [0,0,1,2,0,4,0]] #tener datos de los modelos vendidos
+modelos = [17, 22, 80, 15, 12, 44, 32]
+descripcion_modelos = ['Sentra', 'Jetta', 'Forte', 'Mustang', 'Soul', 'Yaris', 'Camaro']
+cantidad_existencia = [15,10,12,5,7,9,6]
+ventas_modelos = [1,2,1,1,0,6,3]
 
-#total articulos no vendidos
-def print_tot_art_no_vend():
-    for i in articulos:
-        print("Articulos")
-        print(f"{articulos[i]}")
+#1
+#se registra la venta y el vendedor 
+def registrar_venta(): #Funciona
+    #print de los modelos disponibles y seleccion de modelo vendido
+    print('Modelos disponibles')
+    for i in range(len(modelos)):
+        print(modelos[i], end=' ')
+    print()
+    modelo_vendido = int(input('Ingresa el modelo: '))
     
-#agrega articulos
-def agregar_articulos(articulo):
-    if articulo not in articulos:
-        articulos.append(articulo)
-
-#checa si los articulos se repiten, y agrega un contador de cuantos hay en la lista
-def agrega_contador_art_repetidos(articulos):
-        for i in range(len(articulos)):
-            if articulos[i] in articulos[:i]:
-                articulos[i] = articulos[i] + " x " + str(i)
-
-#checa si hay articulos repetidos, los devuelve en una lista,
-#el primer item es la cantidad total de repetidos
-def checa_art_repetidos(articulos):
-    lista_art_repetidos = []
-    contador_art_repetidos = 0
+    #print de la cantidad en existencia y cantidad vendida
+    print('Cantidad en existencia de cada articulo: ')
+    for i in range(len(cantidad_existencia)):
+        print(cantidad_existencia[i], end=' ')
+    print()
+    cantidad_ventas = int(input('Ingresa la cantidad vendida: '))
     
-    for i in range(len(articulos)):
-            if articulos[i] in articulos[:i]:
-                lista_art_repetidos.append(articulos[i])
-                contador_art_repetidos += 1
-    lista_art_repetidos.insert(0,"Total de articulos repetidos: " + contador_art_repetidos)
-    return lista_art_repetidos
-
-
-
-def agregar_vendedor(vendedor):
-    if vendedor not in vendedores:
-        vendedores.append(vendedor)
-
-#para acceder a los pointers i, x necesitas declararlos antes, no funciona
-i = 0
-x = 0
-def agregar_vendedor_articulo(vendedores, articulos):
-    for list in vendedor_articulo:
-        i = 0
-        i += 1
-        x = 0
-        for number in list:
-            x += 1
-            print(vendedor_articulo[i-1][x-1])
-
-#Agrega la cantidad, descripcion y modelo del articulo
-#Si el modelo esta repetido, se actualiza la desc y la cantidad
-#se necesita conocer la cantidad de articulos antes de llamar la funcion,
-            #declara la cantidad antes
-cantidad_articulos = 1
-inventario_articulos = [['Placeholder']]
-def agregar_articulo_inventario():
-    for i in range(cantidad_articulos):
-        modelo = input("Modelo: ")
-        descripcion = input("Descripción: ")
-        cantidad_existencia = input("Cantidad en existencia: ")
+    #ingresa datos del vendedor
+    for i in range(len(nombre_vendedores)):
+        print(nombre_vendedor[i], end=' ')
+    print()
+    vendedor = int(input('Vendedor (0, 1, 2): '))
+    for modelo in range(len(cantidad_existencia)):
+        if modelo_vendido == modelos[modelo]:
+            #checa si la cantidad vendida es mayor a la que esta en existencia
+            if (cantidad_existencia[modelo] - int(cantidad_ventas)) < 0:
+                print('Los datos son incorrectos')
+            else:
+                cantidad_existencia[modelo] = cantidad_existencia[modelo] - int(cantidad_ventas)
+                ventas_modelos[modelo] = int(cantidad_ventas)
+                datos_ventas_vendedor[vendedor][modelo] = int(cantidad_ventas)
+        elif (cantidad_ventas < 0):
+            print('Los datos son incorrectos')
+            break
         
-        if modelo not in inventario_articulo[i-1][:i]:
-            inventario_articulo.append([modelo, descripcion, cantidad_existencia])
-        elif modelo in inventario_articulo[i-1][:i]:
-            inventario_articulo[i-1] = [modelo, descripcion, cantidad_existencia]
-        if inventario_articulo[0][0] == 'Placeholder':
-            inventario_articulo.pop(0)
+    if modelo_vendido not in modelos:
+        print('Los datos son incorrectos')
 
+#2
+#se registra la llegada del modelo
+def registro_llegada_articulo(): #Funciona
+    print('Modelos disponibles')
+    for i in range(len(modelos)):
+        print(modelos[i], end=' ')
+    print()
+    modelo_llegada = int(input('Ingresa el modelo: '))
+    
+    print('Cantidad en existencia de cada articulo: ')
+    for i in range(len(cantidad_existencia)):
+        print(cantidad_existencia[i], end=' ')
+    print()
+    cantidad_llegada = int(input('Cantidad que llegó: '))
+        
+    for modelo in range(len(cantidad_existencia)):
+        if cantidad_llegada < 0:
+            resultado = 'Los datos ingresados son incorrectos'
+            break
+        if modelo_llegada == modelos[modelo]:
+            cantidad_existencia[modelo] += cantidad_llegada
+    #checa si los datos son correctos
+    if modelo_llegada not in modelos:
+        print('Los datos ingresados son incorrectos')
 
-#para agregar la infor de un vendedor, desde 0
-#definir la cantidad de vendedores antes de llamar la función
-cantidad_vendedores = 2
-def agregar_info_vendedor():
-    for i in range(cantidad_vendedores):
-        lista_art_vendedor = []
-        nombre_vendedor = input(f"Nombre del vendedor {i}: ")
-        for x in range(cantidad_articulos):
-            articulo = input(f"Ventas del articulo {inventario_articulos[x][0]}: ")
-            lista_art_vendedor.append(articulo)
-        if nombre_vendedor not in vendedores[i-1][:i]:
-            vendedores.append([nombre_vendedor, lista_art_vendedor])
-            
-    if vendedores[0] == ['Placeholder']:
-        vendedores.pop(0)
+#3
+def consulta_inventario_disponible(): #Funciona
+    print('Modelos:', end=' ')
+    for i in range(len(modelos)):
+        print(modelos[i], end=' ')
+    print()
+
+    print('Descripción:', end=' ')
+    for i in range(len(modelos)):
+        print(descripcion_modelos[i], end=' ')
+    print()
+
+    print('Cantidad en existencia:', end=' ')
+    for i in range(len(modelos)):
+        print(cantidad_existencia[i], end=' ')
+    print()
+
+#4
+def articulo_mas_vendido(): #Funciona
+    best_seller = 0
+    contador = 0
+    for i in range(len(ventas_modelos)):
+        if ventas_modelos[i] > best_seller:
+            best_seller = ventas_modelos[i]
+    for i in range(len(ventas_modelos)):
+        if best_seller == ventas_modelos[i]:
+            contador = i
+    print(f'El modelo más vendido es: {modelos[contador]}, {descripcion_modelos[contador]}, \
+con {ventas_modelos[contador]} ventas')
+
+#5
+def mejor_vendedor(): #Funciona
+    best_seller = ''
+    ventas_final = 0
+    contador = 0
+    for i in range(len(datos_ventas_vendedor)):
+        ventas_temp = 0
+        for x in range(len(datos_ventas_vendedor[i])):
+            ventas_temp += datos_ventas_vendedor[i][x]
+        if ventas_temp > ventas_final:
+            ventas_final = ventas_temp
+            contador = i
+    print(f'El mejor vendedor fue: {nombre_vendedor[contador]}, con {ventas_final} ventas')
+
+#6
+def reporte_ventas(): #Funciona
+    print('Modelo', end=' ')
+    for i in range(len(modelos)):
+        print(modelos[i], end=' ')
+    print()
+
+    for i in range(len(datos_ventas_vendedor)):
+        print(nombre_vendedor[i], end=' ')
+        for x in range(len(datos_ventas_vendedor[i])):
+            print(datos_ventas_vendedor[i][x], end=' ')
+        print()
